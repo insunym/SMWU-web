a5024376593d58e6716b82fda021b1dd6d18c426
kyoungis <164391087+kyoungis@users.noreply.github.com>
2025-06-19 10:09:24 +0900

    Add files via upload

--- final/station.js
+++ final/station.js
@@ -1,19 +1,19 @@
 const stations = [
-    { name: "대곡", img: "/station_data/대곡역.jpg", url: "/station_data/review-daegok.html" },
-    { name: "디지털미디어시티", img: "/station_data/디지털미디어시티역.jpg", url: "/station_data/review-dmc.html" },
-    { name: "홍대입구", img: "/station_data/홍대입구역.jpg", url: "/station_data/review-hongdae.html" },
-    { name: "공덕", img: "/station_data/공덕역.jpg", url: "/station_data/review-gongdeok.html" },
-    { name: "용산", img: "/station_data/용산역.jpg", url: "/station_data/review-yongsan.html" },
-    { name: "이촌", img: "/station_data/이촌역.jpg", url: "/station_data/review-ichon.html" },
-    { name: "서빙고", img: "/station_data/서빙고역.jpg", url: "/station_data/review-seobinggo.html" },
-    { name: "옥수", img: "/station_data/옥수역.jpg", url: "/station_data/review-oksu.html" },
-    { name: "응봉", img: "/station_data/응봉역.jpg", url: "/station_data/review-eungbong.html" },
-    { name: "왕십리", img: "/station_data/왕십리역.jpg", url: "/station_data/review-wangsimni.html" },
-    { name: "청량리", img: "/station_data/청량리역.jpg", url: "/station_data/review-cheongnyangni.html" },
-    { name: "상봉", img: "/station_data/상봉역.jpg", url: "/station_data/review-sangbong.html" },
-    { name: "망우", img: "/station_data/망우역.jpg", url: "/station_data/review-mangwoo.html" },
-    { name: "구리", img: "/station_data/구리역.jpg", url: "/station_data/review-guri.html" },
-    { name: "지평", img: "/station_data/지평역.jpg", url: "/station_data/review-jipyeong.html" }
+    { name: "대곡", img: "대곡역.jpg", url: "review-daegok.html" },
+    { name: "디지털미디어시티", img: "디지털미디어시티역.jpg", url: "review-dmc.html" },
+    { name: "홍대입구", img: "홍대입구역.jpg", url: "review-hongdae.html" },
+    { name: "공덕", img: "공덕역.jpg", url: "review-gongdeok.html" },
+    { name: "용산", img: "용산역.jpg", url: "review-yongsan.html" },
+    { name: "이촌", img: "이촌역.jpg", url: "review-ichon.html" },
+    { name: "서빙고", img: "서빙고역.jpg", url: "review-seobinggo.html" },
+    { name: "옥수", img: "옥수역.jpg", url: "review-oksu.html" },
+    { name: "응봉", img: "응봉역.jpg", url: "review-eungbong.html" },
+    { name: "왕십리", img: "왕십리역.jpg", url: "review-wangsimni.html" },
+    { name: "청량리", img: "청량리역.jpg", url: "review-cheongnyangni.html" },
+    { name: "상봉", img: "상봉역.jpg", url: "review-sangbong.html" },
+    { name: "망우", img: "망우역.jpg", url: "review-mangwoo.html" },
+    { name: "구리", img: "구리역.jpg", url: "review-guri.html" },
+    { name: "지평", img: "지평역.jpg", url: "review-jipyeong.html" }
 ]
 
 function renderStations() {
